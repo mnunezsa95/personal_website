2bc7cd36b14627e20403dc13b6edc2171541232c
mnunezsa <mnunezsa95@gmail.com>
2023-12-01 08:50:20 -0500

    format landing page, add location

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -7,12 +7,12 @@ function Main() {
   return (
     <div>
       <section className="main">
-        <div className="title">
-          <p className="title__text">Hi,</p>
-          <p className="title__text">
-            I'm <img className="title__letter-m" src={letterM} alt="letter m"></img>arlon,
+        <div className="main__title">
+          <p className="main__title-text">Hi,</p>
+          <p className="main__title-text">
+            I'm <img className="main__title-letter-m" src={letterM} alt="letter m"></img>arlon,
           </p>
-          <p className="title__text">a software enginner.</p>
+          <p className="main__title-text">a software enginner.</p>
         </div>
         <div className="main__image-container">
           <img className="main__image" src={mainImage} alt="coding neural network"></img>
--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,14 +1,21 @@
 import React from "react";
-import "./Sidebar.css";
+import { useLocation } from "react-router-dom";
 import profileIcon from "../../images/letter-m.svg";
 import emailIcon from "../../images/email-icon.svg";
 import codeIcon from "../../images/code-icon.svg";
 import homeIcon from "../../images/home-icon.svg";
+import homeIconCurrent from "../../images/home-icon-current.svg";
 import imageIcon from "../../images/image-icon.svg";
 import linkedinIcon from "../../images/linkedin-icon.svg";
 import githubIcon from "../../images/github-icon.svg";
+import "./Sidebar.css";
 
 function Sidebar() {
+  let location = useLocation();
+  const currentLocation = location.pathname;
+  const handleGitHubLink = () => window.open("https://github.com/mnunezsa95");
+  const handleLinkedinLink = () => window.open("https://www.linkedin.com/in/marlon-nunez-35ba43b4/");
+
   return (
     <section className="sidebar">
       <div className="sidebar__name-container">
@@ -16,9 +23,15 @@ function Sidebar() {
       </div>
       <div className="sidebar__navigation">
         <div className="sidebar__navigation-container">
-          <button className="sidebar__navigation-button">
-            <img className="sidebar__navigation-icon" src={homeIcon} alt="home icon"></img>
-          </button>
+          {currentLocation !== "/" ? (
+            <button className="sidebar__navigation-button">
+              <img className="sidebar__navigation-icon" src={homeIcon} alt="home icon"></img>
+            </button>
+          ) : (
+            <button className="sidebar__navigation-button">
+              <img className="sidebar__navigation-icon" src={homeIconCurrent} alt="home icon"></img>
+            </button>
+          )}
           <button className="sidebar__navigation-button">
             <img className="sidebar__navigation-icon" src={imageIcon} alt="imageIcon"></img>
           </button>
@@ -32,10 +45,10 @@ function Sidebar() {
       </div>
       <div className="sidebar__navigation-social-icons">
         <div className="sidebar__navigation-container">
-          <button className="sidebar__navigation-button">
+          <button className="sidebar__navigation-button" onClick={handleLinkedinLink}>
             <img className="sidebar__navigation-icon" src={linkedinIcon} alt="linkedin icon"></img>
           </button>
-          <button className="sidebar__navigation-button">
+          <button className="sidebar__navigation-button" onClick={handleGitHubLink}>
             <img className="sidebar__navigation-icon" src={githubIcon} alt="linkedin icon"></img>
           </button>
         </div>
