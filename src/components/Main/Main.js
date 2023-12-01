3bc864efcad62c3388ee8ee44908608236526cd5
mnunezsa <mnunezsa95@gmail.com>
2023-12-01 09:05:55 -0500

    update landing page

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -5,20 +5,18 @@ import mainImage from "../../images/obsidan-graph-view.png";
 
 function Main() {
   return (
-    <div>
-      <section className="main">
-        <div className="main__title">
-          <p className="main__title-text">Hi,</p>
-          <p className="main__title-text">
-            I'm <img className="main__title-letter-m" src={letterM} alt="letter m"></img>arlon,
-          </p>
-          <p className="main__title-text">a software enginner.</p>
-        </div>
-        <div className="main__image-container">
-          <img className="main__image" src={mainImage} alt="coding neural network"></img>
-        </div>
-      </section>
-    </div>
+    <section className="main">
+      <div className="main__title">
+        <p className="main__title-text">Hi,</p>
+        <p className="main__title-text">
+          I'm <img className="main__title-letter-m" src={letterM} alt="letter m"></img>arlon,
+        </p>
+        <p className="main__title-text">a software enginner.</p>
+      </div>
+      <div className="main__image-container">
+        <img className="main__image" src={mainImage} alt="coding neural network"></img>
+      </div>
+    </section>
   );
 }
 
