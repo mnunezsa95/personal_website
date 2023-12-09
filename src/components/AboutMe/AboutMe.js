d80438838793bf9501b668e2199f2b4e58afb7c6
mnunezsa <mnunezsa95@gmail.com>
2023-12-09 10:30:51 -0500

    update

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import profileImage from "../../images/marlon-nunez.jpeg";
 import "./AboutMe.css";
 import { useHistory } from "react-router-dom";
