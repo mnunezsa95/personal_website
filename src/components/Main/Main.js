a0a88b9979a1f92b34797f730cdea163ca884778
mnunezsa <mnunezsa95@gmail.com>
2023-12-22 17:39:39 -0600

    update

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -12,7 +12,7 @@ const MainBlock = block(function Main() {
         <p className="main__title-text">
           I'm <img className="main__title-letter-m" src={letterM} alt="letter m"></img>arlon,
         </p>
-        <p className="main__title-text">a full-stack enginner.</p>
+        <p className="main__title-text">a full-stack engineer.</p>
       </div>
       <div className="main__image-container">
         <img className="main__image" src={mainImage} alt="coding neural network"></img>
